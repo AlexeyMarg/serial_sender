c0aafd3e008fc2aee2bec4472f6f6175e2531e9e
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2020-06-12 10:24:49 +0300

    Add main and class form

---
+++ form_class.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+
+class form:
+    def __init__(self, parent):
+        self.init_ui()
+        self.init_commands()
+
+    def init_ui(self):
+        pass
+
+    def init_commands(self):
+        pass
---
+++ main.py
@@ -0,0 +1,7 @@
+from form_class import tk, form
+
+root = tk.Tk()
+window = form(root)
+
+if __name__ == '__main__':
+    root.mainloop()
