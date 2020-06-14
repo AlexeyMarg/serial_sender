fc8476def2a34a86ccd7ecac3b310cde637f9196
AlexeyMarg <65703985+AlexeyMarg@users.noreply.github.com>
2020-06-14 13:55:32 +0300

    serial commands

--- form_class.py
+++ form_class.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 import serial
+import serial.tools.list_ports
 
 
 class form:
     def __init__(self, parent):
         self.parent = parent
+        self.ser = serial.Serial()
         self.init_ui()
         self.init_commands()
 
@@ -14,7 +16,9 @@ class form:
 
         self.port_label = tk.Label(self.port_frame, text='Port')
         self.port_label.grid(row=1, column=1)
-        temp = self.port_list()
+        temp = ['None']
+        if len(self.port_list()) != 0:
+            temp.append(self.port_list())
         self.var_port = tk.StringVar(self.parent)
         self.var_port.set(temp[0])
         self.port_option = tk.OptionMenu(self.port_frame, self.var_port, *temp)
@@ -80,7 +84,7 @@ class form:
         self.dsrdtr_check = tk.Checkbutton(self.port_frame, variable=self.var_dsrdtr)
         self.dsrdtr_check.grid(row=3, column=9)
 
-        self.writetimeout_label = tk.Label(self.port_frame, text = 'Write \ntimeout')
+        self.writetimeout_label = tk.Label(self.port_frame, text='Write \ntimeout')
         self.writetimeout_label.grid(row=1, column=10)
         self.var_writetimeout = tk.BooleanVar(self.parent)
         self.var_writetimeout.set(1)
@@ -91,7 +95,7 @@ class form:
         self.writetimeout_entry = tk.Entry(self.port_frame, width=6, bg='lightgrey')
         self.writetimeout_entry.grid(row=3, column=10)
 
-        self.interbytetimeout_label = tk.Label(self.port_frame, text = 'Write inter \n byte timeout')
+        self.interbytetimeout_label = tk.Label(self.port_frame, text='Write inter \n byte timeout')
         self.interbytetimeout_label.grid(row=1, column=12)
         self.var_interbytetimeout = tk.BooleanVar(self.parent)
         self.var_interbytetimeout.set(1)
@@ -144,7 +148,7 @@ class form:
         self.addmessage_entry.grid(row=8, column=4)
 
     def read_commands(self):
-        commands=[
+        commands = [
             ['Send 1', '1'],
             ['Send H', 'H']
         ]
@@ -152,7 +156,7 @@ class form:
             self.command_listbox.insert('end', str(i[0]))
 
     def read_messages(self):
-        commands=[
+        commands = [
             ['Send 1', '1'],
             ['Send H', 'H']
         ]
@@ -166,6 +170,7 @@ class form:
                 self.timeout_entry.config(bg='white')
             else:
                 self.timeout_entry.config(bg='lightgrey')
+
         self.timeout_check.bind('<Button-1>', click_timeout)
 
         def click_writetimeout(event):
@@ -174,6 +179,7 @@ class form:
                 self.writetimeout_entry.config(bg='white')
             else:
                 self.writetimeout_entry.config(bg='lightgrey')
+
         self.writetimeout_check.bind('<Button-1>', click_writetimeout)
 
         def click_interbytetimeout(event):
@@ -182,76 +188,121 @@ class form:
                 self.interbytetimeout_entry.config(bg='white')
             else:
                 self.interbytetimeout_entry.config(bg='lightgrey')
+
         self.interbytetimeout_check.bind('<Button-1>', click_interbytetimeout)
 
         def click_xonxoff(event):
             self.disconnect()
+
         self.xonxoff_check.bind('<Button-1>', click_xonxoff)
 
         def click_rtscts(event):
             self.disconnect()
+
         self.rtscts_check.bind('<Button-1>', click_rtscts)
 
         def click_dsrdtr(event):
             self.disconnect()
+
         self.dsrdtr_check.bind('<Button-1>', click_dsrdtr)
 
         def click_connect(event):
             self.connect()
+
         self.connect_button.bind('<Button-1>', click_connect)
 
         def click_disconnect(event):
             self.disconnect()
+
         self.disconnect_button.bind('<Button-1>', click_disconnect)
 
         def click_command(event):
             i = self.command_listbox.curselection()
             self.message_listbox.selection_clear(0, 'end')
             self.message_listbox.selection_set(i[0])
+
         self.command_listbox.bind('<Double-Button-1>', click_command)
 
         def click_message(event):
             i = self.message_listbox.curselection()
             self.command_listbox.selection_clear(0, 'end')
             self.command_listbox.selection_set(i[0])
+
         self.message_listbox.bind('<Double-Button-1>', click_message)
 
         def click_port(event):
             self.disconnect()
+
         self.port_option.bind('<Button-1>', click_port)
 
         def click_baud(event):
             self.disconnect()
+
         self.baud_option.bind('<Button-1>', click_baud)
 
         def click_databits(event):
             self.disconnect()
+
         self.databits_option.bind('<Button-1>', click_databits)
 
         def click_stopbits(event):
             self.disconnect()
+
         self.stop_option.bind('<Button-1>', click_stopbits)
 
         def click_parity(event):
             self.disconnect()
+
         self.parity_option.bind('<Button-1>', click_parity)
 
     def port_list(self):
-        s = [
-            'COM1',
-            'COM2',
-            'COM3'
-        ]
+        # s = ['COM1', 'COM2', 'COM3']
+        s = serial.tools.list_ports.comports(include_links=False)
         return s
 
     def connect(self):
-        pass
+        if self.var_port.get() != 'None':
+            if self.var_timeout.get() == 1:
+                self.timeout = None
+            else:
+                self.timeout = float(self.timeout_entry.get())
+            if self.var_writetimeout.get() == 1:
+                self.writetimeout = None
+            else:
+                self.writetimeout = float(self.writetimeout_entry.get())
+            if self.var_interbytetimeout.get() == 1:
+                self.interbytetimeout = None
+            else:
+                self.interbytetimeout = float(self.interbytetimeout_entry.get())
+            self.ser = serial.Serial(
+                port=self.var_port.get(),
+                baudrate=int(self.var_baud.get()),
+                bytesize=int(self.var_databits.get()),
+                parity=eval('serial.' + self.var_parity.get()),
+                stopbits=int(self.var_stop.get()),
+                timeout=self.timeout,
+                xonxoff=self.xonxoff_check.get(),
+                rtscts=self.rtscts_check.get(),
+                dsrdtr=self.dsrdtr_check.get(),
+                write_timeout=self.writetimeout,
+                inter_byte_timeout=self.interbytetimeout
+            )
+
+            if self.ser.isOpen():
+                self.connection_label.config(text='Connected', fg='Green')
 
     def disconnect(self):
-        pass
+        if self.ser in locals():
+            if self.ser.isOpen():
+                self.ser.close()
+                self.connection_label.config(text='Disconnected', fg='Red')
 
     def send(self):
-        pass
+        if self.ser in locals():
+            if self.ser.isOpen():
+                s = self.message_listbox.curselection()
+                s = self.message_listbox.get(s)
+                self.ser.write(s)
 
     def add(self):
         s = self.addcommand_entry.get()
@@ -265,35 +316,35 @@ class form:
         self.command_listbox.delete('active')
 
     baudrates = [
-        '110',
-        '300',
-        '600',
-        '1200',
-        '2400',
-        '4800',
-        '9600',
-        '19200',
-        '38400',
-        '57600',
+        '110   ',
+        '300   ',
+        '600   ',
+        '1200  ',
+        '2400  ',
+        '4800  ',
+        '9600  ',
+        '19200 ',
+        '38400 ',
+        '57600 ',
         '115200',
         '128000',
         '256000',
     ]
-    bytesizes=[
+    bytesizes = [
         '5',
         '6',
         '7',
         '8'
     ]
     parities = [
-        'None',
-        'Even',
-        'Odd',
-        'Mark',
+        'None ',
+        'Even ',
+        'Odd  ',
+        'Mark ',
         'Space'
     ]
     stopbits = [
-        '1',
+        '1  ',
         '1.5',
-        '2'
+        '2  '
     ]
